416197a3ca0d29885c6a9a771b47dfb39215afa4
Ben Bryant <brbryant2639@gmail.com>
2021-09-08 08:22:29 -0500

    Adding iframe for video carousel p1

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
 console.log("Script Attached");
 
+const iframeLinks = [
+  "https://www.youtube.com/embed/UBJ5QDsHkBg",
+  "https://www.youtube.com/embed/EbI0sTNdE-o",
+  "https://www.youtube.com/embed/HjzPdJBZylI",
+  "https://www.youtube.com/embed/YK5Qtrm2xDI",
+  "https://www.youtube.com/embed/ZJSfM91L3R8",
+  "https://www.youtube.com/embed/lS301BBNqt4",
+  "https://www.youtube.com/embed/YdEsfPeSxy8",
+];
+
 function toggleSideNavItem(e, ele) {
   document
     .querySelectorAll(".nav-link")
